7e1ac246101b78b8b2f70f68696c1ff1440dca50
lbo728 <extreme93@naver.com>
2026-01-21 16:10:51 +0900

    style(Skeleton): adjust style

--- components/skeleton/PostDetailSkeleton.tsx
+++ components/skeleton/PostDetailSkeleton.tsx
@@ -29,28 +29,11 @@ export function PostDetailSkeleton() {
               <Separator className="my-8" />
 
               <div className="prose-skeleton space-y-4">
+                <Skeleton className="h-6 w-full" />
+                <Skeleton className="h-6 w-full" />
                 <Skeleton className="h-6 w-1/3" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-5/6" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-4/5" />
-                <Skeleton className="h-6 w-2/5 mt-8" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-3/4" />
-                <Skeleton className="h-32 w-full rounded-lg mt-4" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-5/6" />
               </div>
             </article>
-
-            <Separator className="my-12" />
-
-            <nav className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <Skeleton className="h-24 rounded-lg" />
-              <Skeleton className="h-24 rounded-lg" />
-            </nav>
           </div>
         </div>
       </div>
